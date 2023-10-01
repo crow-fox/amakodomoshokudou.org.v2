2965480f4bab84cf28603ee0563fc95c1e972503
crow-fox <surakamiya198@gmail.com>
2023-10-01 11:55:13 +0900

    正しく画像が読み込まれるように修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: import.meta.env.PROD
+    ? "https://amakodomoshokudou.org"
+    : "http://localhost:4321",
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
+  build: {
+    assets: "assets",
+    inlineStylesheets: `never`,
+  },
+});
