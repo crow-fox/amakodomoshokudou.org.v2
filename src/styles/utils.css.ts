d5ef0061a075ca02c28ce6ccd03453f6c42ae745
crow-fox <surakamiya198@gmail.com>
2023-10-01 16:38:04 +0900

    TextLinkのCSSをVanilla-extractに変更

---
+++ src/styles/utils.css.ts
@@ -0,0 +1,10 @@
+export const textLinkMedia = {
+  "(hover: hover)": {
+    ":hover": {
+      boxShadow: "0 0 0 2px white, 0 0 0 4px var(--c-blue)",
+    },
+  },
+  "(prefers-color-scheme: dark)": {
+    color: "var(--c-blue-light)",
+  },
+};
