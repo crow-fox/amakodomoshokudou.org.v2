d04d4c3e8dae80f1b4a63099b2db5cfd04d3c158
crow-fox <surakamiya198@gmail.com>
2023-10-01 10:41:36 +0900

    Globalな定数

---
+++ src/consts/site.ts
@@ -0,0 +1,4 @@
+export const site = {
+  title: "尼崎こども食堂ネットワーク",
+  cafesPerPage: 8,
+} as const;
---
+++ src/consts/summary.ts
@@ -0,0 +1,40 @@
+export const summary = {
+  name: {
+    title: "団体名",
+    value: "尼崎こども食堂ネットワーク",
+  },
+  constituent: {
+    title: "構成団体",
+    value: [
+      "社会福祉法人尼崎市社会福祉協議会",
+      "認定NPO法人子どものみらい尼崎",
+      "NPO法人スマイルひろば",
+    ],
+  },
+  establish: {
+    title: "設立",
+    value: {
+      datetime: "2019",
+      display: "2019年",
+    },
+  },
+  representative: {
+    title: "代表",
+    value: "濱田格子",
+  },
+  location: {
+    title: "所在地",
+    value: {
+      postalCode: "661-0033",
+      address: "兵庫県尼崎市南武庫之荘3-24-5 尼社協ほっと館内",
+    },
+  },
+  facebook: {
+    title: "Facebook",
+    value: "https://facebook.com/amakodomoshokudou/",
+  },
+  mail: {
+    title: "メールアドレス",
+    value: "ama.kodomoshokudou@gmail.com",
+  },
+} as const;
