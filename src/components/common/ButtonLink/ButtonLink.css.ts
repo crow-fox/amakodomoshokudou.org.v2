55d3fd724c5d50aa383e90d746dcae17a7c02873
crow-fox <surakamiya198@gmail.com>
2023-10-01 16:59:47 +0900

    vanilla-extractで宣言されたcssはstylesオブジェクト形式でimportするように変更

--- src/components/common/ButtonLink/ButtonLink.css.ts
+++ src/components/common/ButtonLink/ButtonLink.css.ts
@@ -1,23 +1,25 @@
 import { cssVars } from "@/styles/app.css";
 import { style } from "@vanilla-extract/css";
 
-export const link = style({
-  display: "inline-grid",
-  gridTemplateColumns: "1fr auto",
-  columnGap: ".5rem",
-  alignItems: "center",
-  padding: "1rem",
-  fontWeight: 700,
-  color: cssVars.color.blueDark,
-  textDecoration: "none",
-  backgroundColor: cssVars.color.orangeLight,
-  border: `2px solid ${cssVars.color.orange}`,
-  borderRadius: 2,
-  "@media": {
-    "(prefers-color-scheme: dark)": {
-      color: cssVars.color.gray,
-      backgroundColor: cssVars.color.orangeDarkest,
-      borderColor: cssVars.color.orangeDark,
+export default {
+  link: style({
+    display: "inline-grid",
+    gridTemplateColumns: "1fr auto",
+    columnGap: ".5rem",
+    alignItems: "center",
+    padding: "1rem",
+    fontWeight: 700,
+    color: cssVars.color.blueDark,
+    textDecoration: "none",
+    backgroundColor: cssVars.color.orangeLight,
+    border: `2px solid ${cssVars.color.orange}`,
+    borderRadius: 2,
+    "@media": {
+      "(prefers-color-scheme: dark)": {
+        color: cssVars.color.gray,
+        backgroundColor: cssVars.color.orangeDarkest,
+        borderColor: cssVars.color.orangeDark,
+      },
     },
-  },
-});
+  }),
+};
