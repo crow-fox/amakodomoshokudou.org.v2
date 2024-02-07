cc7e5e05970c77dd7e969eaf1aaf37d8dbf5846e
crow-fox <surakamiya198@gmail.com>
2024-02-07 15:23:35 +0900

    tailwind.configに今後追加したい設定のコメントを記述

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx,astro,html}"],
   theme: {
+    colors: {
+      // カラーテーマを登録（ダークモード対応を加味）
+      // pluginでbodyにstyleを追加することを検討
+    },
+    spacing: {
+      // スペースを登録
+      // フィボナッチ数列と4の倍数を基準にする
+    },
+    fontFamily: {
+      // フォントを登録
+      // システムフォントとGoogleフォントを基準にする
+      // 元のフォントを継承した上で、一番優先のフォントをGoogleフォントにする
+    },
+    fontSize: {
+      // フォントサイズとline-heightを登録
+      // 本文のサイズのみline-heightを広めも指定できるようにしたい。
+    },
     extend: {},
   },
   plugins: [],
