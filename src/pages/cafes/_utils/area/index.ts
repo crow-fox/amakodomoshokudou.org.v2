775cc1bdea99357e98638e2f31610e98f93fa1f2
crow-fox <surakamiya198@gmail.com>
2024-07-01 21:40:10 +0900

    fetchAreasをmock

--- src/pages/cafes/_utils/area/index.ts
+++ src/pages/cafes/_utils/area/index.ts
@@ -1,16 +1,29 @@
+import { mockOrFetch } from "@/mock";
 import { newtClient } from "@/pages/_libs/newt";
 import { parseArea } from "@/pages/_schemas/area";
+import { areasFixture } from "@/pages/cafes/_utils/area/fixture";
 
-export async function fetchAreas() {
+async function realFetchAreas() {
   const { items } = await newtClient.getContents({
     appUid: "cafes",
     modelUid: "area",
     query: {
-      select: ["_id", "_sys", "name", "slug", "priority"],
+      select: ["_id", "name", "slug", "priority"],
       order: ["priority"],
     },
   });
 
   const areas = items.map((item) => parseArea(item));
   return areas;
+}
+
+function mockFetchAreas(): Awaited<ReturnType<typeof realFetchAreas>> {
+  return areasFixture;
+}
+
+export async function fetchAreas() {
+  return await mockOrFetch({
+    fetcher: realFetchAreas,
+    mock: mockFetchAreas,
+  });
 }
