22f04b8c8e10951f66f4abb9ffa53cff7a4564f3
crow-fox <surakamiya198@gmail.com>
2024-07-01 21:38:55 +0900

    areaFixtureFactory

---
+++ src/pages/cafes/_utils/area/fixture.ts
@@ -0,0 +1,74 @@
+import type { NewtArea } from "@/pages/_schemas/area";
+import { literal, parse, union, type InferOutput } from "valibot";
+
+const mockAreaIdSchema = union([
+  literal("1"),
+  literal("2"),
+  literal("3"),
+  literal("4"),
+  literal("5"),
+  literal("6"),
+]);
+
+export function isMockAreaId(value: unknown) {
+  return parse(mockAreaIdSchema, value);
+}
+
+export type MockAreaId = InferOutput<typeof mockAreaIdSchema>;
+
+export function areaFixtureFactory(areaId: MockAreaId): NewtArea {
+  switch (areaId) {
+    case "1": {
+      return {
+        _id: "1",
+        name: "武庫",
+        slug: "muko",
+        priority: 1,
+      };
+    }
+    case "2": {
+      return {
+        _id: "2",
+        name: "立花",
+        slug: "tachibana",
+        priority: 2,
+      };
+    }
+    case "3": {
+      return {
+        _id: "3",
+        name: "園田",
+        slug: "sonoda",
+        priority: 3,
+      };
+    }
+    case "4": {
+      return {
+        _id: "4",
+        name: "大庄",
+        slug: "osho",
+        priority: 4,
+      };
+    }
+    case "5": {
+      return {
+        _id: "5",
+        name: "中央",
+        slug: "chuo",
+        priority: 5,
+      };
+    }
+    case "6": {
+      return {
+        _id: "6",
+        name: "小田",
+        slug: "oda",
+        priority: 6,
+      };
+    }
+    default: {
+      const _: never = areaId;
+      throw new Error(`Unknown areaId: ${_}`);
+    }
+  }
+}
